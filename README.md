c94d3debfb5341debc218c74e585053f168fa4c3
AshithaL <you@example.com>
2020-07-20 07:50:43 +0530

    added blockers in readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MGNREGA--JAVA
+
+## Blockers
+
+- In Db_connection.java
+
+After running the file, I get the below mentioned error
+
+```
+Error: Main method not found in class mgnrega.Db_connection, please define the main method as:
+   public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+```
--- src/mgnrega/Db_connection.java
+++ src/mgnrega/Db_connection.java
@@ -1,22 +1,21 @@
 package mgnrega;
 
-import java.sql.*;  
+import java.sql.*;
 
 public class Db_connection {
-	public static void main(String args[]) {
-		try{  
-			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con=DriverManager.getConnection(  
-			"jdbc:mysql://localhost:3306/mgnrega?autoReconnect=true&useSSL=false","root","nineleaps");  
-	        System.out.println("Connection is created succcessfully");
-
-			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery("show tables"); 
-			System.out.println("The tables in mgnrega db are -");
-			while (rs.next() ) 
-			System.out.println(rs.getString("Tables_in_mgnrega"));
-			con.close();  
-			}catch(Exception e){ System.out.println(e);}  
+	public Connection con;
+	
+	public Connection connect() {
+	Connection con = null;
+	try{  
+		Class.forName("com.mysql.jdbc.Driver");  
+		con=DriverManager.getConnection(  
+		"jdbc:mysql://localhost:3306/mgnrega?autoReconnect=true&useSSL=false","root","nineleaps");  
+	    System.out.println("Connection is created succcessfully");
+	    return con;
+	    }catch(Exception e){ System.out.println(e);}
+	
+		return null; 		
 	}
 
 }
